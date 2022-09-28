3dd8499f9694db21f2d111d4d5ab1ae08f14fe4b
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2022-09-27 19:31:18 -0700

    get-one route done

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,12 +1,14 @@
 const { uuid } = require("uuidv4");
 var express = require("express");
 var router = express.Router();
+const id = uuid()
 
 const { db } = require("../mongo");
 
-router.get("/get-one-example", async function (req, res, next) {
+try {
+router.get('/get-one-example', async function (req, res, next) {
   const blogPost = await db()
-    .collection("blogPosts")
+    .collection('blogPosts')
     .findOne({
       id: {
         $exists: true,
@@ -17,5 +19,38 @@ router.get("/get-one-example", async function (req, res, next) {
     post: blogPost,
   });
 });
+} catch (err) {
+    console.log(err.name)
+        res.json({
+            success: false,
+            error: err.toString()
+        })
+}
+
+// try {
+    router.get('/get-one/:id', async function (req, res, next) {
+        const blogId = req.params.id
+        
+        const blogPost = await db()
+        .collection('blogPosts')
+        .findOne({
+            id: blogId
+        })
+        res.json({
+            success: true,
+            post: blogPost
+        })
+    })
+// } catch (err) {
+//     console.log("hello")
+//     res.json({
+//         success: false,
+//         error: err.toString()
+//     })
+// }
+
+
+
+
 
 module.exports = router
